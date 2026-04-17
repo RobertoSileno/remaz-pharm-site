45c308ba53c432c18d39e8e10b3419158d49f525
Roberto Sileno <robertosileno.1@gmail.com>
2026-04-17 00:13:04 -0400

    adicionando register

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, login_view
+from .views import home, login_view, register
 
 urlpatterns = [
     path('', home, name='home'),
     path('login/', login_view, name='login'),
+    path('register/', register, name='register'),
 ]
